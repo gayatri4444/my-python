cf3f38f1ff2f33285c6dcc4c5ed6f47da2cebc82
gayatri4444 <gayatring44@gmail.com>
2025-11-10 16:31:37 +0530

    Create arithmatic.py

---
+++ arithmatic.py
@@ -0,0 +1,15 @@
+#arthmatic operation
+a = int(input("enter number 1"))
+num2 = int(input("enter number 2"))
+add = a+num2
+sub = a-num2
+div1 = a/num2
+div2 = a//num2
+div3 = a%num2
+mul= a*num2
+print(add)
+print(sub)
+print(div1)
+print(div2)
+print(div3)
+print(mul) 
