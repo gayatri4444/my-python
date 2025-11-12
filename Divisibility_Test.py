522a4144f78fabeb3dd6307a7e8e240cbc354f91
gayatri4444 <gayatring44@gmail.com>
2025-11-12 22:01:18 +0530

    Add divisibility test for two numbers

---
+++ Divisibility_Test.py
@@ -0,0 +1,6 @@
+#Divisibility Test
+num1 = int(input("enter a number"))
+num2 = int(input("enter another number"))
+if num1%3==0 and num1%5==0:
+  print(f"{num1} is divisible by both 3 and 5")
+  print(f"{num2} is divisible by both 3 and 5")
