4112defa3a70189fb6b12f378bb52f715137f6ae
gayatri4444 <gayatring44@gmail.com>
2025-11-13 16:12:05 +0530

    Create list_creat.py

---
+++ list_creat.py
@@ -0,0 +1,8 @@
+a = [1,2,3,4,5,6] # creating list
+print(a)
+print(a[3]) #acessing the items
+print(a[-2]) #reversing index
+a[2] = 22 #modifying the items
+a.append(88)  #adding last
+a.insert(1, 88)   #add at specific place
+print(a)
